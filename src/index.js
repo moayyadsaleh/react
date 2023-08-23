9cf02d5820d8c1883a3abfc0889f9d9fc96e49af
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-23 14:42:15 -0600

    Add some styling

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
 import React from "react";
-import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // Import createRoot from the correct path
 import"../src/index.css"; //Import Global styles
 import './components/styles.css'; // Import component-specific styles
 
-ReactDOM.render(
-  <div className="heading">
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Bacon</li>
-      <li>Jamon</li>
-      <li>Noodles</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
+
+
+const img = "https://picsum.photos/200";
+const rootElement = document.getElementById("root");
+
+// Use createRoot instead of ReactDOM.render
+//Name HTML attributes with Camel Case as if it is JS, example: className
+//in the html attributes, Java script can be used to insert, example: <img src={img} > </img>
+const root = createRoot(rootElement);
+root.render(
+  <div>
+    <h1 className="heading" contenteditable="true" spellCheck="true">My Favourite Foods</h1> 
+    <p title="I'm a tooltip">This is a paragraph.</p>
+    <p lang="fr">Ceci est un paragraphe.</p>
+    <p translate="yes">Don't translate this!</p>
+<div className="food-image">
+
+<img src={img}></img>
+</div>
+  </div>
 );
