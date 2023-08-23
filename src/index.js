1174441ab7c3bbd10d2962be6613089a3f5e8e65
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-23 08:01:31 -0600

    Import React and write some code

--- src/index.js
+++ src/index.js
@@ -1,17 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+import ReactDOM from "react-dom";
+//Inject HTML code inside JS code.
+//React uses Babel complier that changes the react code and next Generation JS into Vanilla JS so that any browser can process the code
+ReactDOM.render(<div><h1>Hello World</h1><p>This is a paragraph</p></div>, document.getElementById("root"));
