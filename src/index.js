493354550ce5daf4a5642666e58ed5534dcdbb5d
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-23 11:42:02 -0600

    import styles to the app, and fix up some styling

--- src/index.js
+++ src/index.js
@@ -1,19 +1,16 @@
-
 import React from "react";
-import ReactDOM  from "react-dom";
+import ReactDOM from "react-dom";
+import"../src/index.css"; //Import Global styles
+import './components/styles.css'; // Import component-specific styles
 
-const creatorName = "Moayyad";
-const creationYear = new Date().getFullYear();
-
-ReactDOM.render(<div>
-<p>created by {creatorName}</p>
-<p>&copy; {creationYear}. All rights reserved.</p>
-</div>, document.getElementById("root"))
-//Create a react app from scratch.
-//It should display 2 paragraph HTML elements.
-//The paragraphs should say:
-//Created by YOURNAME.
-//Copyright CURRENTYEAR.
-//E.g.
-//Created by Angela Yu.
-//Copyright 2019.
+ReactDOM.render(
+  <div className="heading">
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Jamon</li>
+      <li>Noodles</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
