de92d1b23c9dd4633e606f0e84a0374b37b6f26d
moayyadsaleh <moayyad.al-azzam@umontana.edu>
2023-08-23 21:42:07 -0600

    Practice creating components

--- src/index.js
+++ src/index.js
@@ -2,20 +2,22 @@
 //Name HTML attributes with camelCase as if it is JS, example: className
 //in the html attributes, Java script can be used to insert, example: <img src={img} > </img>
 //React follows Pascal convention naming, the first word is a capital letter
+//The Index JS renders the components tree
+//JS function can be used as elements(Custom components)
 
 import React from "react";
 import { createRoot } from "react-dom/client";
 import"../src/index.css"; //Import Global styles
 import './components/styles.css'; // Import component-specific styles
-import App from "./components/App"
+import './components/App'; // Import component-specific styles
+import Heading from "./components/Heading";
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
 
-//JS function can be used as elements(Custom components)
 
 
 
 root.render(
-<App></App>
+<Heading></Heading>
 );
